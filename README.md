4312faec5ba436e42605030df77e3fe867775321
Alex Taber <aft.pokemon@gmail.com>
2016-05-14 22:07:40 -0400

    Menuhax support and reboot support

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ If you use a custom path, create a update.cfg file in /luma/ and put your comple
 
 This currently supports custom paths and path changing patch, and both hourlies and stable releases! In addition, it supports making and restoring backups of arm9loaderhax.bin!
 
+Before updating, it shows you your current Luma version and the latest available Luma version
+
+Recent features allow menuhax support as well. This will always extract it to /Luma3DS.dat on the root of your SD card due to path changer limitations. If you want backup support with menuhax, set your update.cfg file to this location.
+
 ## Credits
  * Thanks to Rinnegatamante for lpp-3ds, which this depends upon
  * Thanks to Aurora Wright for the awesome CFW
--- index.lua
+++ index.lua
@@ -1,16 +1,16 @@
 local white = Color.new(255,255,255)
 local hourlyUrl = "http://astronautlevel2.github.io/Luma3DS/latest.zip"
 local stableUrl = "http://astronautlevel2.github.io/Luma3DS/release.zip"
-local a9lh_path = "/arm9loaderhax.bin" --max length 38 characters including the first slash. if bigger, the path inside the .bin wont be changed
+local payload_path = "/arm9loaderhax.bin" --max length 38 characters including the first slash. if bigger, the path inside the .bin wont be changed
 local zip_path = "/Luma3DS.zip"
-local backup_path = a9lh_path..".bak"
+local backup_path = payload_path..".bak"
 local remoteVer = "http://astronautlevel2.github.io/Luma3DS/lastVer"
  
 function readConfig(fileName)
     if (System.doesFileExist(fileName)) then
         local file = io.open(fileName, FREAD)
-        a9lh_path = io.read(file, 0, io.size(file))
-        backup_path = a9lh_path..".bak"
+        payload_path = io.read(file, 0, io.size(file))
+        backup_path = payload_path..".bak"
     end
 end
 
@@ -45,48 +45,59 @@ function getVer(path)
 end
  
 function path_changer()
-    local file = io.open(a9lh_path, FREAD)
+    local file = io.open(payload_path, FREAD)
     local a9lh_data = io.read(file, 0, io.size(file))
     io.close(file)
     local offset = string.find(a9lh_data, "%"..unicodify("arm9loaderhax.bin"))
-    local new_path = unicodify(string.sub(a9lh_path,2,-1))
+    local new_path = unicodify(string.sub(payload_path,2,-1))
     if #new_path < 74 then
         for i = 1,74-#new_path,1 do
             new_path = new_path..string.char(00)
         end
-        local file = io.open(a9lh_path, FWRITE)
+        local file = io.open(payload_path, FWRITE)
         io.write(file, offset-1, new_path, 74)
         io.close(file)
     end
 end
 
-function update(site)
+function update(site, isMenuhax)
     Screen.refresh()
     Screen.clear(TOP_SCREEN)
     Screen.waitVblankStart()
     Screen.flip()
     if Network.isWifiEnabled() then
-        Screen.debugPrint(5,5, "Update path: "..a9lh_path, white, TOP_SCREEN)
         Network.downloadFile(site, zip_path)
-        Screen.debugPrint(5,20, "File downloaded!", white, TOP_SCREEN)
-        Screen.debugPrint(5,35, "Backing up arm9loaderhax.bin...", white, TOP_SCREEN)
+        Screen.debugPrint(5,5, "File downloaded!", white, TOP_SCREEN)
+        Screen.debugPrint(5,20, "Backing up payload", white, TOP_SCREEN)
         if (System.doesFileExist(backup_path)) then
             System.deleteFile(backup_path)
         end
-        System.renameFile(a9lh_path, backup_path)
-        System.extractFromZIP(zip_path, "out/arm9loaderhax.bin", a9lh_path)
-        Screen.debugPrint(5,50, "Moving to payload location...", white, TOP_SCREEN)
-        System.deleteFile(zip_path)
-        Screen.debugPrint(5,65, "Changing path for reboot patch", white, TOP_SCREEN)
-        path_changer()
-        Screen.debugPrint(5,80, "Done!", white, TOP_SCREEN)
-        Screen.debugPrint(5,95, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
+        System.renameFile(payload_path, backup_path)
+        if (isMenuhax == false) then
+            System.extractFromZIP(zip_path, "out/arm9loaderhax.bin", payload_path)
+            Screen.debugPrint(5,35, "Moving to payload location...", white, TOP_SCREEN)
+            System.deleteFile(zip_path)
+            Screen.debugPrint(5,50, "Changing path for reboot patch", white, TOP_SCREEN)
+            path_changer()
+        elseif (isMenuhax == true) then
+            System.extractFromZIP(zip_path, "out/Luma3DS.dat", "/Luma3DS.dat")
+            System.deleteFile(zip_path)
+        end
+        Screen.refresh()
+        Screen.clear(TOP_SCREEN)
+        Screen.waitVblankStart()
+        Screen.flip()
+        Screen.debugPrint(5,5, "Done!", white, TOP_SCREEN)
+        Screen.debugPrint(5,20, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
+        Screen.debugPrint(5,35, "Press SELECT to reboot", white, TOP_SCREEN)
         while true do
             pad = Controls.read()
             if Controls.check(pad,KEY_START) then
                 Screen.waitVblankStart()
                 Screen.flip()
                 System.exit()
+            elseif Controls.check(pad,KEY_SELECT) then
+                System.reboot()
             end
         end
         
@@ -111,14 +122,14 @@ function main()
     Screen.debugPrint(5,20, "Press A to update stable Luma3DS", white, TOP_SCREEN)
     Screen.debugPrint(5,35, "Press X to update unstable Luma3DS", white, TOP_SCREEN)
     Screen.debugPrint(5,50, "Press B to restore a Luma3DS backup", white, TOP_SCREEN)
-    Screen.debugPrint(5,65, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
-    Screen.debugPrint(5,80, "Your Luma3DS version: "..getVer(a9lh_path), white, TOP_SCREEN)
-    Screen.debugPrint(5,95, "Latest Luma3DS version: "..Network.requestString(remoteVer), white, TOP_SCREEN)
+    Screen.debugPrint(5,65, "Press Y to update to latest stable Menuhax", white, TOP_SCREEN)
+    Screen.debugPrint(5,80, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
+    Screen.debugPrint(5,95, "Your Luma3DS version: "..getVer(payload_path), white, TOP_SCREEN)
+    Screen.debugPrint(5,110, "Latest Luma3DS version: "..Network.requestString(remoteVer), white, TOP_SCREEN)
     Screen.debugPrint(5,140, "Thanks to:", white, TOP_SCREEN)
-    Screen.debugPrint(5,155, "astro and ericchu for the builds", white, TOP_SCREEN)
-    Screen.debugPrint(5,170, "Aurora Wright for her amazing CFW", white, TOP_SCREEN)
-    Screen.debugPrint(5,185, "Rinnegatamante for lpp-3ds", white, TOP_SCREEN)
-    Screen.debugPrint(5,200, "Hamcha for the idea", white, TOP_SCREEN)
+    Screen.debugPrint(5,155, "Aurora Wright for her amazing CFW", white, TOP_SCREEN)
+    Screen.debugPrint(5,170, "Rinnegatamante for lpp-3ds", white, TOP_SCREEN)
+    Screen.debugPrint(5,185, "Hamcha for the idea", white, TOP_SCREEN)
     Screen.waitVblankStart()
     Screen.flip()
     while true do
@@ -128,9 +139,11 @@ function main()
             Screen.flip()
             System.exit()
         elseif Controls.check(pad,KEY_A) then
-            update(stableUrl)
+            update(stableUrl, false)
         elseif Controls.check(pad,KEY_X) then
-            update(hourlyUrl)
+            update(hourlyUrl, false)
+        elseif Controls.check(pad,KEY_Y) then
+            update(stableUrl, true)
         elseif Controls.check(pad,KEY_B) then
             Screen.refresh()
             Screen.clear(TOP_SCREEN)
@@ -138,9 +151,9 @@ function main()
             Screen.flip()
             if System.doesFileExist(backup_path) then
                 Screen.debugPrint(5,5, "Deleting new arm9loaderhax.bin...", white, TOP_SCREEN)
-                System.deleteFile(a9lh_path)
+                System.deleteFile(payload_path)
                 Screen.debugPrint(5,20, "Renaming backup to arm9loaderhax.bin...", white, TOP_SCREEN)
-                System.renameFile(backup_path, a9lh_path)
+                System.renameFile(backup_path, payload_path)
                 Screen.debugPrint(5,35, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
                 while true do
                     pad = Controls.read()

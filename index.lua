57b6b3d7cecee87381d25488ca7aefd83a4deb48
Alex Taber <aft.pokemon@gmail.com>
2016-05-15 18:08:35 -0400

    New features

--- index.lua
+++ index.lua
@@ -1,11 +1,15 @@
 local white = Color.new(255,255,255)
 local hourlyUrl = "http://astronautlevel2.github.io/Luma3DS/latest.zip"
 local stableUrl = "http://astronautlevel2.github.io/Luma3DS/release.zip"
-local payload_path = "/arm9loaderhax.bin" --max length 38 characters including the first slash. if bigger, the path inside the .bin wont be changed
+local hourlyDevUrl = "http://astronautlevel2.github.io/Luma3DSDev/latest.zip"
+local stableDevUrl = "http://astronautlevel2.github.io/Luma3DSDev/release.zip"
+local payload_path = "/arm9loaderhax.bin"
 local zip_path = "/Luma3DS.zip"
 local backup_path = payload_path..".bak"
 local remoteVer = "http://astronautlevel2.github.io/Luma3DS/lastVer"
+local latestCIA = "http://astronautlevel2.github.io/Luma3DS/Updater.CIA"
 local isMenuhax = false
+local isDev = false
 
 function readConfig(fileName)
     if (isMenuhax) then
@@ -24,12 +28,26 @@ function readConfig(fileName)
     end
 end
 
+function sleep(n)
+  local timer = Timer.new()
+  local t0 = Timer.getTime(timer)
+  while Timer.getTime(timer) - t0 <= n do end
+end
+
 function getMode()
+    local updateMode = ""
     if (isMenuhax) then
-        return "Menuhax"
+        updateMode = "Menuhax"
     else
-        return "a9lh"
+        updateMode = "a9lh"
     end
+    if (isDev) then
+        updateMode = updateMode..", dev"
+    else
+        updateMode = updateMode..", normal"
+    end
+
+    return updateMode
 end
 
 function unicodify(str)
@@ -144,58 +162,84 @@ function main()
     Screen.debugPrint(5,35, "Press X to update unstable Luma3DS", white, TOP_SCREEN)
     Screen.debugPrint(5,50, "Press B to restore a Luma3DS backup", white, TOP_SCREEN)
     Screen.debugPrint(5,65, "Press Y to switch between a9lh and menuhax", white, TOP_SCREEN)
-    Screen.debugPrint(5,80, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
-    Screen.debugPrint(5,95, "Your Luma3DS version: "..getVer(payload_path), white, TOP_SCREEN)
-    Screen.debugPrint(5,110, "Latest Luma3DS version: "..Network.requestString(remoteVer), white, TOP_SCREEN)
-    Screen.debugPrint(5,125, "Current mode: "..getMode(), white, TOP_SCREEN)
+    Screen.debugPrint(5,80, "Press SELECT to switch between dev and normal", white, TOP_SCREEN)
+    Screen.debugPrint(5,95, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
+    Screen.debugPrint(5, 110, "Press L to update the updater", white, TOP_SCREEN)
+    Screen.debugPrint(5,125, "Your Luma3DS version: "..getVer(payload_path), white, TOP_SCREEN)
+    Screen.debugPrint(5,140, "Latest Luma3DS version: "..Network.requestString(remoteVer), white, TOP_SCREEN)
+    Screen.debugPrint(5,155, "Current mode: "..getMode(), white, TOP_SCREEN)
     if (not isMenuhax) then
-        Screen.debugPrint(5, 140, "Install dir: "..payload_path, white, TOP_SCREEN)
+        Screen.debugPrint(5, 170, "Install dir: "..payload_path, white, TOP_SCREEN)
     end
     Screen.waitVblankStart()
     Screen.flip()
     while true do
         pad = Controls.read()
-        if Controls.check(pad,KEY_START) then
-            Screen.waitVblankStart()
-            Screen.flip()
-            System.exit()
-        elseif Controls.check(pad,KEY_A) then
-            update(stableUrl)
-        elseif Controls.check(pad,KEY_X) then
-            update(hourlyUrl)
-        elseif Controls.check(pad,KEY_Y) then
-            isMenuhax = not isMenuhax
-            readConfig("/luma/update.cfg")
-            Screen.clear(TOP_SCREEN)
-            main()
-        elseif Controls.check(pad,KEY_B) then
-            Screen.refresh()
-            Screen.clear(TOP_SCREEN)
-            Screen.waitVblankStart()
-            Screen.flip()
-            if System.doesFileExist(backup_path) then
-                Screen.debugPrint(5,5, "Deleting new arm9loaderhax.bin...", white, TOP_SCREEN)
-                System.deleteFile(payload_path)
-                Screen.debugPrint(5,20, "Renaming backup to arm9loaderhax.bin...", white, TOP_SCREEN)
-                System.renameFile(backup_path, payload_path)
-                Screen.debugPrint(5,35, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
-                while true do
-                    pad = Controls.read()
+        if pad ~= oldPad then
+            oldPad = pad
+            if Controls.check(pad,KEY_START) then
+                Screen.waitVblankStart()
+                Screen.flip()
+                System.exit()
+            elseif Controls.check(pad,KEY_A) then
+                if (not isDev) then
+                    update(stableUrl)
+                else
+                    update(stableDevUrl)
+                end
+            elseif Controls.check(pad,KEY_X) then
+                if (not isDev) then
+                    update(hourlyUrl)
+                else
+                    update(hourlyDevUrl)
+                end
+            elseif Controls.check(pad,KEY_Y) then
+                isMenuhax = not isMenuhax
+                readConfig("/luma/update.cfg")
+                Screen.clear(TOP_SCREEN)
+                main()
+            elseif Controls.check(pad,KEY_SELECT) then
+                isDev = not isDev
+                Screen.clear(TOP_SCREEN)
+                main()
+            elseif Controls.check(pad,KEY_L) then
+                Screen.clear(TOP_SCREEN)
+                Screen.debugPrint(5, 5, "Downloading new CIA...", white, TOP_SCREEN)
+                Network.downloadFile(latestCIA, "/Updater.CIA")
+                sleep(2000)
+                Screen.debugPrint(5, 20, "Installing CIA...", white, TOP_SCREEN)
+                System.installCIA("/Updater.CIA", SDMC)
+                System.deleteFile("/Updater.CIA")
+                System.exit()
+            elseif Controls.check(pad,KEY_B) then
+                Screen.refresh()
+                Screen.clear(TOP_SCREEN)
+                Screen.waitVblankStart()
+                Screen.flip()
+                if System.doesFileExist(backup_path) then
+                    Screen.debugPrint(5,5, "Deleting new arm9loaderhax.bin...", white, TOP_SCREEN)
+                    System.deleteFile(payload_path)
+                    Screen.debugPrint(5,20, "Renaming backup to arm9loaderhax.bin...", white, TOP_SCREEN)
+                    System.renameFile(backup_path, payload_path)
+                    Screen.debugPrint(5,35, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
+                    while true do
+                        pad = Controls.read()
+                            if Controls.check(pad,KEY_START) then
+                                Screen.waitVblankStart()
+                                Screen.flip()
+                                System.exit()
+                        end
+                    end
+                else
+                    Screen.debugPrint(5,5, "Backup path: "..backup_path, white, TOP_SCREEN)
+                    Screen.debugPrint(5,20, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
+                    while true do
+                        pad = Controls.read()
                         if Controls.check(pad,KEY_START) then
                             Screen.waitVblankStart()
                             Screen.flip()
                             System.exit()
-                    end
-                end
-            else
-                Screen.debugPrint(5,5, "Backup path: "..backup_path, white, TOP_SCREEN)
-                Screen.debugPrint(5,20, "Press START to go back to HBL/Home menu", white, TOP_SCREEN)
-                while true do
-                    pad = Controls.read()
-                    if Controls.check(pad,KEY_START) then
-                        Screen.waitVblankStart()
-                        Screen.flip()
-                        System.exit()
+                        end
                     end
                 end
             end

a1024c3be1f692e26bf549e98c4aa3f7a5c81874
alex <alex@ahsraiderreview.com>
2016-05-29 23:55:01 -0400

    Rewrite continues

--- index.lua
+++ index.lua
@@ -1,3 +1,54 @@
+--StarUpdater by astronautlevel (Alex Taber) and TurtleP
+--This project is copyright 2016
+--Licensed under MIT license
+
+local Colors =
+{
+	white = Color.new(209, 213, 216, 255),
+	green = Color.new(65, 168, 95, 255),
+	red = Color.new(184, 49, 47, 255),
+	blue = Color.new(97, 202, 187, 255),
+	black = Color.new(117, 112, 107, 186),
+	darkBlue = Color.new(41, 105, 176, 255),
+	darkGreen = Color.new(25, 53, 49, 255)
+}
+
+local background = Graphics.loadImage("/assets/background.png")
+local modeFont = Font.load("/assets/HeyGorgeous.ttf")
+local updateFont = Font.load("/assets/HeyGorgeous.ttf")
+Font.setPixelSizes(updateFont, 18)
+Font.setPixelSizes(updateFont, 12)
+
+local isDev = false
+local isMenuhax = false
+local isStable = true
+
+local lumaUpdaterText =
+{
+	status = "No update is available",
+	color = Colors.red
+}
+
+local lumaVersion = "Luma3DS "
+
+local function keyIsDown(button)
+	return Controls.check(Controls.read(), button)
+end
+
+local getLumaVersion()
+	if System.doesFileExist("/arm9loaderhax.bin") then
+		local file = io.open("/arm9loaderhax.bin", FREAD)
+
+		local info = file:read(0, file:size())
+
+		file:close()
+
+		if info:find("Developer") then
+			isDeveloper = true
+		end
+		
+		
+
 
 function toUnicode(str)
 	local newString = ""
